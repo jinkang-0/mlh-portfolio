57660da4c5b04eac2573a2ae63f65d18df0fd345
jinkang-0 <jinkang.fang@gmail.com>
2025-06-30 22:32:15 +0000

    add redeployment script

---
+++ redeploy-site.sh
@@ -0,0 +1,20 @@
+# kill tmux sessions
+tmux kill-session
+
+# cd into project folder
+cd /root/mlh-portfolio
+
+# ensure repo is up-to-date
+git fetch
+git checkout main
+git reset origin/main --hard
+
+# enter python virtual env
+source python3-virtualenv/bin/activate
+
+# install python repositories
+pip install -r requirements.txt
+
+# start new detached tmux session
+tmux new -s portfolio 'flask run --host=0.0.0.0'
+
