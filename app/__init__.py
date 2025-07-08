dc5b8123a05b33134d91cfc44d10fc1deb151841
jinkang-0 <jinkang.fang@gmail.com>
2025-07-08 12:11:55 -0700

    add mysql, add test script

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,39 @@
 import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
+from peewee import MySQLDatabase, Model, CharField, TextField, DateTimeField
+from playhouse.shortcuts import model_to_dict
+import datetime
 
 load_dotenv()
 app = Flask(__name__)
+
+# database creation
+mydb = MySQLDatabase(
+  os.getenv('MYSQL_DATABASE'),
+  user=os.getenv('MYSQL_USER'),
+  password=os.getenv('MYSQL_PASSWORD'),
+  host=os.getenv('MYSQL_HOST'),
+  port=3306
+)
+
+# define models
+class TimelinePost(Model):
+  name = CharField()
+  email = CharField()
+  content = TextField()
+  created_at = DateTimeField(default=datetime.datetime.now)
+
+  class Meta:
+    database = mydb
+
+# connect and attach models
+mydb.connect()
+mydb.create_tables([TimelinePost])
+
+
+
+# app variables
 
 experiences = [
   {
@@ -28,6 +58,9 @@ experiences = [
 
 title = "Jinkang Fang"
 
+
+# page routes
+
 @app.route('/')
 def index():
   return render_template('index.html', title=title, experiences=experiences, url=os.getenv("URL"))
@@ -35,3 +68,28 @@ def index():
 @app.route('/hobbies')
 def hobbies():
   return render_template('hobbies.html', title=title, url=os.getenv("URL"))
+
+
+# api routes
+
+@app.route('/api/timeline_post', methods=['POST'])
+def post_timeline_post():
+  name = request.form['name']
+  email = request.form['email']
+  content = request.form['content']
+  timeline_post = TimelinePost.create(name=name, email=email, content=content)
+
+  return model_to_dict(timeline_post)
+
+@app.route('/api/timeline_post', methods=['GET'])
+def get_timeline_posts():
+  return {
+    'timeline_posts': [model_to_dict(p) for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())]
+  }
+
+@app.route('/api/timeline_post', methods=['DELETE'])
+def delete_timeline_post():
+  post_id = request.form['id']
+  post = TimelinePost.get(TimelinePost.id == post_id)
+  post.delete_instance()
+  return {'status': 'success', 'message': 'Post deleted successfully'}
